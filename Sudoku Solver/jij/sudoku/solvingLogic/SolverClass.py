1ce57ae6f06ec4f7e0f99b8c4793ef22b8601045
jeejz <34750480+jeejz@users.noreply.github.com>
2018-11-16 18:05:18 +0530

    Added logic for removal of a number from a position

--- Sudoku Solver/jij/sudoku/solvingLogic/SolverClass.py
+++ Sudoku Solver/jij/sudoku/solvingLogic/SolverClass.py
@@ -1,49 +1,87 @@
-import numpy as np
 
-charar = np.zeros(shape=[5, 5],dtype='S9')
-charar.fill("123456789")
-print(charar)
+defAllPossibleValues = '123456789'
 
-te = str(charar[1][1])
-f,s,t = te.rpartition('2')
-te = f+t
-print(te.startswith())
-charar[1,1]=te
-print('------')
-print(charar)
+matrix = [[defAllPossibleValues for i in range(9)] for j in range(9)]
 
 
-def generateGroupKeysForKey(key):
+def remove_invalid_number_from_location(num, loc):
+    """ To remove a non possible number from the possiblities."""
+    x,y = int(loc[0]),int(loc[1])
+    f,s,t = matrix[x][y].rpartition(str(num))
+    matrix[x][y] = f+t
+
+
+def remove_value_at_position(num,loc):
+    x, y = int(loc[0]), int(loc[1])
+    matrix[x][y] = ''
+
+
+def pretty_print_matrix(matrix):
+    s = [[str(e) for e in row] for row in matrix]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print ('\n'.join(table))
+
+
+def identify_least_sized_elems(m):
+    pass
+    #lens = [max(map(len, col)) for col in zip(*s)]
+
+print(min(matrix[1], key=len))
+
+s = [[str(e) for e in row] for row in matrix]
+lens = [min(map(len, col)) for col in zip(*s)]
+print(lens)
+
+def generate_group_keys_for_key(key):
     grpKeyList = []
-    for i in [1,4,7]:
+    for i in [0,3,6]:
         if i <= int(key[0]) < i+3 :
             cKeyX = int((i + i + 2)/2)
         if i <= int(key[1]) < i+3 :
             cKeyY = int((i + i + 2)/2)
-
     for k in [cKeyX-1, cKeyX, cKeyX+1]:
         for l in [cKeyY - 1, cKeyY, cKeyY + 1]:
             grpKeyList.append(str(k)+str(l))
-
     return grpKeyList
 
-def generateColKeysForKey(key):
+
+def generate_col_keys_for_key(k):
     colGrpKeysList = []
-    for i in range(1, 10):
-        colGrpKeysList.append(key[0]+str(i))
+    for i in range(0, 9):
+        colGrpKeysList.append(k[0]+str(i))
     return colGrpKeysList
 
-def generateRowKeysForKey(key):
+
+def generate_row_keys_for_key(key):
     rowGrpKeysList = []
-    for i in range(1,10):
+    for i in range(0,9):
         rowGrpKeysList.append(str(i)+key[1])
     return rowGrpKeysList
 
+
+def conv_to_zero_base(key):
+    return str((int(key[0])-1))+str(int(key[1])-1)
+
+
+def conv_to_one_base(key):
+    return str((int(key[0])+1))+str(int(key[1])+1)
+
+
+def apply_value_at_position(v,pos):
+    conv_key = conv_to_zero_base(pos)
+    [remove_invalid_number_from_location(v,e) for e in generate_col_keys_for_key(conv_key)]
+    [remove_invalid_number_from_location(v,e) for e in generate_row_keys_for_key(conv_key)]
+    [remove_invalid_number_from_location(v,e) for e in generate_group_keys_for_key(conv_key)]
+    remove_value_at_position(v,conv_key)
+
+
 key = '24'
-print(generateGroupKeysForKey(key))
-print(generateColKeysForKey(key))
-print(generateRowKeysForKey(key))
 
-defaultAvailableValues = [[[1,2,3,4,5,6,7,8,9]]]
 
-print (defaultAvailableValues)
+apply_value_at_position(3,'45')
+apply_value_at_position(6,'89')
+apply_value_at_position(7,'11')
+apply_value_at_position(1,'29')
+pretty_print_matrix(matrix)
